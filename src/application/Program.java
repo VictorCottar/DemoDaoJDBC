333463d055c2649f9fa3571368bf0c6af809476c
VictorCottar <victorcottarms@outlook.com>
2023-10-09 16:25:30 -0300

    Reutilizando a Instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,5 +8,13 @@ public class Program {
 
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("TESTE 1: SELLER FINDBYID ");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
+
+
     }
 }
