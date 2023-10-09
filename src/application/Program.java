3afab7e92aa9b7a5a675a1f2c96fb2baf86a2200
VictorCottar <victorcottarms@outlook.com>
2023-10-09 15:31:39 -0300

    Interface Department e Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,5 @@
 package application;
 
-import model.entities.Department;
-import model.entities.Seller;
-
-import java.util.Date;
-
 public class Program {
 
     public static void main(String[] args) {
